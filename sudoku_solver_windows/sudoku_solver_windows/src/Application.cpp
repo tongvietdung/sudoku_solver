495d50bb79eb6bd37ad6306b251d78769e2efaa7
tongvietdung <vietdung.tong1998@gmail.com>
2022-07-26 01:19:02 +0200

    Input UI
    Create input ui and added docking viewports: input and output

--- sudoku_solver_windows/sudoku_solver_windows/src/Application.cpp
+++ sudoku_solver_windows/sudoku_solver_windows/src/Application.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <iostream>
+#include <string>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -6,7 +9,58 @@
 const unsigned int SCR_WIDTH = 700;
 const unsigned int SCR_HEIGHT = 600;
 
+static char sudoku_text[9 * 9][2];
+static std::vector<std::vector<int>> sudoku;
+static int solve_count = 0;
+
+void Solve() {
+    solve_count++;
+}
+
+void show_sudoku_input() {
+    ImGui::Begin("Sudoku Input");
+        ImGui::BeginTable("sudoku_input", 9);
+        int row, col;
+        for (int cell = 0; cell < 9 * 9; cell++)
+        {
+            row = cell / 10;
+            col = cell % 9;
+
+            ImGui::TableNextColumn();
+            ImGui::PushID(cell);
+            ImGui::InputText("##cell", sudoku_text[cell], IM_ARRAYSIZE(sudoku_text[cell]));
+            if (sudoku_text[cell][0] == '\0')
+            {
+                sudoku[row][col] = 0;
+            }
+            else {
+                sudoku[row][col] = sudoku_text[cell][0] - '0';
+            }
+
+            ImGui::PopID();
+        }
+        ImGui::EndTable();
+
+        if (ImGui::Button("Solve Sudoku")) {
+            std::cout << "Solve!" << std::endl;
+            Solve();
+        }
+    ImGui::End();
+}
+void Init() {
+    for (int row = 0; row < 9; row++)
+    {
+        std::vector<int> temp;
+        for (int col = 0; col < 9; col++)
+        {
+            temp.push_back(0);
+        }
+        sudoku.push_back(temp);
+    }
+}
+
 int main() {
+    Init();
 	// Setup Window
 	if (!glfwInit()) return 1;
 
@@ -16,15 +70,17 @@ int main() {
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Sudoku Solver", NULL, NULL);
 	if (window == NULL) return 1;
+    glfwSetWindowPos(window, 500, 200);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1); // Enable Vsync
+
 
 	// Setup Imgui
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	ImGui::StyleColorsDark();
 
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -32,8 +88,13 @@ int main() {
 
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.0f);
 
+    ImFont* myfont =  io.Fonts->AddFontFromFileTTF(".\\dependencies\\imgui\\misc\\fonts\\Roboto-Medium.ttf", 20.0f);
+    ImGuiStyle style = ImGui::GetStyle();
+    //style.ScaleAllSizes(ImGui::GetFontSize());
+
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
+
 		glfwPollEvents();
 
 		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
@@ -43,9 +104,88 @@ int main() {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
+        #pragma region Docking setup
+        static bool opt_fullscreen = true;
+        static bool opt_padding = false;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace", nullptr, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("Options"))
+            {
+                // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                // which we can't undo at the moment without finer window depth/z control.
+                ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+                ImGui::MenuItem("Padding", NULL, &opt_padding);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
+                if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+                if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
+                if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+                if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+                ImGui::Separator();
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::End();
+        #pragma endregion Docking setup
+
+        show_sudoku_input();
+
 		ImGui::Begin("Testing");
-			ImGui::Text("Testing completed");
+			ImGui::Text(std::to_string(solve_count).c_str());
 		ImGui::End();
+
 
 		ImGui::Render();
 		int display_w, display_h;
---
+++ sudoku_solver_windows/sudoku_solver_windows/src/SA.cpp
@@ -0,0 +1,5 @@
+#include "algorithms\SA.h"
+
+namespace SA 
+{
+}
